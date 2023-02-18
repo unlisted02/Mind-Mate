e4cfd5ac78288171788f55d07c6a02c78db9aafd
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-02-19 02:52:17 +0530

    feat: home page designed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
 import Head from 'next/head';
 import MainLayout from '@/layouts/MainLayout';
-import { Container } from '@nextui-org/react';
+import Lottie from 'lottie-react';
+import heroImage from '../../public/mediHeroImage.json';
+import { Container, Text, Button } from '@nextui-org/react';
+import { useRouter } from 'next/router';
+import styles from '../styles/index.module.css';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClickChat = () => {
+    router.push('/chat');
+  };
   return (
     <>
       <Head>
@@ -10,32 +19,23 @@ export default function Home() {
       </Head>
       <MainLayout>
         <Container>
-          <h1>Home Page</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-            placeat unde sequi sed veritatis autem obcaecati voluptate ut quae
-            provident eligendi quod odio error porro quas explicabo, dignissimos
-            fuga quis minus ipsum eaque totam perferendis nihil. Et ipsa
-            excepturi eos! Quas totam sed omnis similique, corrupti tempora! Ad
-            praesentium ipsa quas atque mollitia, quam et doloremque laborum
-            amet sequi aut consequuntur voluptatibus odit numquam unde
-            laudantium dolores cum sit iure culpa vitae. Quasi enim iure quo!
-            Temporibus asperiores neque, sit earum deleniti at dolorem ipsum
-            dolores porro quasi ipsa dolorum soluta expedita debitis alias
-            libero voluptas est eveniet quos maiores corrupti magni placeat
-            cumque atque? Suscipit, impedit soluta aut totam corrupti cum
-            nostrum quidem earum nulla sit iste nihil assumenda ratione
-            voluptatem aliquam non sunt odit quam. Exercitationem voluptate
-            neque tenetur possimus nemo rerum fugiat iusto quam aperiam tempore
-            vitae, veritatis eius aspernatur! Iure aut vitae vel, porro sequi
-            quod animi possimus nostrum debitis optio, nemo adipisci voluptate
-            recusandae saepe dolorum modi! Ea quod dolore aut reiciendis!
-            Aliquam, pariatur minus qui illo magnam eius velit, suscipit
-            perferendis quae, accusantium ex nisi nam. Ab, numquam quisquam
-            rerum cupiditate exercitationem voluptatem vitae quas consectetur
-            officiis. Mollitia, natus temporibus. Itaque quo fugiat soluta
-            dolore esse.
-          </p>
+          <div className={styles.container}>
+            <div className={styles.heroText}>
+              <Text className={styles.headerTitle} weight='bold'>
+                Unlock the Power of Your Mind with Our AI-Driven Mental Health
+                Web Application
+              </Text>
+              <Text className={styles.subHeading}>
+                Get personalized mental health solutions based on your unique
+                needs and preferences.
+              </Text>
+
+              <Button className={styles.tryBtn} onClick={handleClickChat}>
+                Try It Now
+              </Button>
+            </div>
+            <Lottie className={styles.heroImage} animationData={heroImage} />
+          </div>
         </Container>
       </MainLayout>
     </>
