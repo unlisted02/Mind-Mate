1959e5a2a29ea580e50a5196dfae8ff469ec32cc
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-03-21 12:50:41 +0530

    feat: progress bar added to user routes between pages.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css';
 import { NextUIProvider } from '@nextui-org/react';
+import NextNProgress from 'nextjs-progressbar';
 
 function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
+      <NextNProgress height={5} />
       <Component {...pageProps} />
     </NextUIProvider>
   );
